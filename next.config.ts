ad807630b3b361fd96c159207c903bd762b97ded
Overcomer Emiator <56355878+Xceldeveloper@users.noreply.github.com>
2025-06-12 00:08:37 +0100

    fix: add environment variable configuration for GOOGLE_SHEET_URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GOOGLE_SHEET_URL: process.env.GOOGLE_SHEET_URL,
+  },
 };
 
 export default nextConfig;
