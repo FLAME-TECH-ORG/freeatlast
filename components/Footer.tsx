c7670ccadcd35dea49f1a1ed6f0c4b110d9f9ee3
Overcomer Emiator <56355878+Xceldeveloper@users.noreply.github.com>
2025-06-11 15:04:11 +0100

    feat: enhance footer component with company info, actions, and social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,67 @@
+import { FaFacebookF, FaSquareInstagram } from "react-icons/fa6";
+
 export default function AboutUs() {
   return (
     <div className="footer">
       <div className="footer__content">
-        <div className="footer__content-admonishion">
-          <div className="footer__content-admonishion-text">
-            <p className="merriweather-font">
-              “Because true freedom doesn’t come from just breaking addiction—it
-              comes from rebuilding a whole life and at Free At Last, we don’t
-              sell hope. We give it freely. So... can we help everyone? Maybe
-              not.But we can help this one. And maybe the next. And the
-              next.Partner with us. Help someone start again. One day at a time,
-              for one full year”
-            </p>
-          </div>
+        <div className="footer__content-company">
+          <img
+            src="logo-white.svg"
+            alt="Company Logo"
+            className="footer__content-company-logo"
+          />
+          <p className="footer__content-company-description">
+            A faith-based, transformative rehabilitation center dedicated to
+            restoring hope, rebuilding lives, and renewing purpose.
+          </p>
+        </div>
+        <div className="footer__content-actions">
+          <h4 className="footer__content-actions-title">What We Do</h4>
+          <ul className="footer__content-actions-list">
+            <li className="footer__content-actions-list-item">
+              <span>Daily Structure And Support</span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>Balanced Nutritious Meals</span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>Mental Health Therapy </span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>Professional Medical And Clinical Care</span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>Spiritual Growth and Guidance </span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>Skill Acquisition And Vocational Training</span>
+            </li>
+            <li className="footer__content-actions-list-item">
+              <span>
+                A Safe And Loving Environment To Enhance Wholeness And A Fresh
+                Start
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__content-socials">
+          <h4 className="footer__content-socials-title">Follow Us</h4>
+          <ul className="footer__content-socials-list">
+            <li className="footer__content-socials-list-item">
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaFacebookF />
+              </a>
+            </li>
+            <li className="footer__content-socials-list-item">
+              <a
+                href="https://www.instagram.com/falrehabfoundation"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaSquareInstagram />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
