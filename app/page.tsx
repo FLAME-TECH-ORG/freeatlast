892f5ec54485cc093bbb4a7e9df44176988575a0
Overcomer Emiator <56355878+Xceldeveloper@users.noreply.github.com>
2025-06-12 11:01:54 +0100

    fix: integrate Lenis for smooth scrolling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import AboutUs from "../components/AboutUs";
 import Admission from "../components/Admission";
 import AnchorScripture from "../components/AnchorScripture";
@@ -10,8 +13,19 @@ import Partnership from "../components/Partnership";
 import Testimonies from "../components/Testimonies";
 import WhoWeAre from "../components/WhoWeAre";
 import styles from "./home-page.module.scss";
+import { initLenis } from "../utils/lenis";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = initLenis();
+
+    return () => {
+      // Cleanup Lenis instance on unmount
+      if (lenis) {
+        lenis.destroy();
+      }
+    };
+  }, []);
   return (
     <div className={styles.home}>
       <IndexHero />
